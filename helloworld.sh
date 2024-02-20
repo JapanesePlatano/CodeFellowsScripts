914d8bf75880a1ad4086fcd940cbf3dfe8b92595
JapanesePlatano <JapanesePlatano@github.com>
2024-02-20 15:12:04 -0800

    test

---
+++ helloworld.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Script Name:                  Platano
+# Author:                       Gilbert Collado
+# Date of latest revision:      02/20/2024
+# Purpose:                      Educational
+
+# Declaration of variables
+Food=mangu
+color=brown
+
+# Declaration of functions
+Function display_info() 
+	echo “food: $1”
+	echo “color: $$2”
+}
+
+# Main
+Echo “Better than mashed potatoes!”
+
+# Accessing Variables
+Echo “The $food is that $color.”
+
+# Calling a function
+Display_info “$food” “$color”
+
+
+# End
