191b52f661e9b9c605f409ae8d7a0a18ebe0aa4f
JapanesePlatano <JapanesePlatano@github.com>
2024-02-29 13:38:47 -0800

    challenge08.bat

--- challenge08.bat
+++ challenge08.bat
@@ -1,6 +1,6 @@
 # Script Name:                  Ops Challenge - Windows Batch Scripting
 # Author:                       Gilbert Collado
-# Date of latest revision:      02/27/2024
+# Date of latest revision:      02/28/2024
 # Purpose:                      Create a batch script that robocopies files to another folder via powershell
 # Source1:                      https://g.co/gemini/share/166aeefee93f
 # Source2:                      https://www.makeuseof.com/tag/use-windows-batch-file-commands-automate-repetitive-tasks/
