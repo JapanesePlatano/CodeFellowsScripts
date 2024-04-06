03438379b5ec60a70c3284ea08dc7d943cf12aa9
JapanesePlatano <gilbertcolladoxd@gmail.com>
2024-04-05 22:05:21 -0700

    challengeclass10.py

---
+++ Ops301/challengeclass10.py
@@ -0,0 +1,23 @@
+# Script Name:                  Ops Challenge - Python File Handling
+# Author:                       Gilbert Collado
+# Date of latest revision:      04/05/2024
+# Purpose:                      Create a script that uses file handling in python
+# Source1:                      https://github.com/codefellows/seattle-ops-301d12/tree/main/class-10/challenges
+# Source2:                      https://github.com/codefellows/seattle-ops-301d12/tree/main/class-10/challenges
+
+# Create a new .txt file
+file_name = 'example.txt'
+with open(file_name, 'w') as file:
+    file.write("First line\n")
+    file.write("Second line\n")
+    file.write("Third line\n")
+
+# Read and print the first line
+with open(file_name, 'r') as file:
+    first_line = file.readline()
+    print("First line from the file:", first_line)
+
+# Delete the .txt file
+import os
+os.remove(file_name)
+print("File", file_name, "deleted successfully.")
